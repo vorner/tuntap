c3bb5b2cfbed3af90f7e2ad6a0332dab4304cc11
Federico Martinez <fedemartinezdev@gmail.com>
2022-08-05 11:40:08 +0200

    Adds support for Mac OS
    Tested on macOS Monterrey 12.5

--- src/tuntap.c
+++ src/tuntap.c
@@ -8,8 +8,16 @@
 #include <string.h>
 
 #include <sys/socket.h>
+#ifdef __linux__
 #include <linux/if.h>
 #include <linux/if_tun.h>
+#else
+#include <net/if.h>
+#define IFF_TUN 0x0001
+#define IFF_TAP 0x0002
+#define IFF_NO_PI 0x1000
+#define TUNSETIFF _IOW('T', 202, int)
+#endif
 #include <sys/ioctl.h>
 
 /**
