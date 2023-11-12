09599b86855d169929994ccc10cdb5900671c81b
XIX-tech <siberianrednecks@gmail.com>
2023-11-12 14:18:10 +0000

    tokio v1 API

--- examples/tokio.rs
+++ examples/tokio.rs
@@ -8,24 +8,25 @@
 //!
 //! You really do want better error handling than all these unwraps.
 
-extern crate futures;
-extern crate tokio_core;
-extern crate tun_tap;
-
+use bytes::Bytes;
+use futures_util::stream::StreamExt;
+use futures_util::SinkExt;
 use std::process::Command;
 use std::time::Duration;
+use tokio::time::sleep;
+use tokio_util::codec::{BytesCodec, Decoder};
 
-use futures::{Future, Stream};
-use tokio_core::reactor::{Core, Interval};
-use tun_tap::{Iface, Mode};
 use tun_tap::r#async::Async;
+use tun_tap::{Iface, Mode};
 
 /// The packet data. Note that it is prefixed by 4 bytes ‒ two bytes are flags, another two are
 /// protocol. 8, 0 is IPv4, 134, 221 is IPv6. <https://en.wikipedia.org/wiki/EtherType#Examples>.
-const PING: &[u8] = &[0, 0, 8, 0, 69, 0, 0, 84, 44, 166, 64, 0, 64, 1, 247, 40, 10, 107, 1, 2, 10,
-    107, 1, 3, 8, 0, 62, 248, 19, 160, 0, 2, 232, 228, 34, 90, 0, 0, 0, 0, 216, 83, 3, 0, 0, 0, 0,
-    0, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
-    39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55];
+const PING: &[u8] = &[
+    0, 0, 8, 0, 69, 0, 0, 84, 44, 166, 64, 0, 64, 1, 247, 40, 10, 107, 1, 2, 10, 107, 1, 3, 8, 0,
+    62, 248, 19, 160, 0, 2, 232, 228, 34, 90, 0, 0, 0, 0, 216, 83, 3, 0, 0, 0, 0, 0, 16, 17, 18,
+    19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42,
+    43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55,
+];
 
 /// Run a shell command. Panic if it fails in any way.
 fn cmd(cmd: &str, args: &[&str]) {
@@ -35,29 +36,30 @@ fn cmd(cmd: &str, args: &[&str]) {
         .unwrap()
         .wait()
         .unwrap();
-    assert!(ecode.success(), "Failed to execte {}", cmd);
+    assert!(ecode.success(), "Failed to execute {}", cmd);
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let iface = Iface::new("testtun%d", Mode::Tun).unwrap();
     eprintln!("Iface: {:?}", iface);
     // Configure the „local“ (kernel) endpoint. Kernel is (the host) 10.107.1.3, we (the app)
     // pretend to be 10.107.1.2.
     cmd("ip", &["addr", "add", "dev", iface.name(), "10.107.1.3/24"]);
     cmd("ip", &["link", "set", "up", "dev", iface.name()]);
-    let mut core = Core::new().unwrap();
-    let iface = Async::new(iface, &core.handle()).unwrap();
-    let (sink, stream) = iface.split();
-    let writer = Interval::new(Duration::from_secs(1), &core.handle())
-        .unwrap()
-        .map(|_| {
+
+    let iface = Async::new(iface).unwrap();
+    let (mut sink, mut stream) = BytesCodec::new().framed(iface).split();
+
+    tokio::spawn(async move {
+        loop {
+            sleep(Duration::from_secs(1)).await;
             println!("Sending ping");
-            PING.to_owned()
-        })
-        .forward(sink);
-    let reader = stream.for_each(|packet| {
-        println!("Received: {:?}", packet);
-        Ok(())
+            let _ = sink.send(Bytes::from(PING)).await;
+        }
     });
-    core.run(reader.join(writer)).unwrap();
+
+    while let Some(Ok(packet)) = stream.next().await {
+        println!("Received: {:?}", packet);
+    }
 }
